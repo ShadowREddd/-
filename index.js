6cb2513b9c79e4459cf00e6e9f41fb0696ccdc73
ShadowREddd <leo2001724@gmail.com>
2025-12-11 21:03:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function handleEvent(event) {
   else {
       replyText = '您好！您可以輸入「超市」來逛逛我們的商店，或是詢問「營業時間」。';
   }
-replyText = replyText.replace(/[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/gu, '');
+
   // 回傳文字訊息
   return client.replyMessage(event.replyToken, {
     type: 'text',
@@ -61,4 +61,5 @@ app.listen(port, () => {
   console.log(`listening on ${port}`);
 
 });
+
 
